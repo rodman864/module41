1a36b6aba31d0e324073a83aa7034edb57081c30
rodman864 <59933094+rodman864@users.noreply.github.com>
2021-01-21 18:50:08 -0700

    Add files via upload

---
+++ Module4/module3/MyClasses/WeddingCake.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace module3.MyClasses
+{
+    public class WeddingCake 
+    {
+        public void MyMethod<T>(T myparameter)
+        {
+
+            Console.WriteLine("My favorite flavor is vanilla");
+
+        }
+        public class MyClasses
+        {
+
+
+
+        }
+        public class Vanilla
+        {
+            public string MyProperty { get; set; }
+        }
+
+
+
+}
+
+}
